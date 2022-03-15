871ceec4608453b8376930d6200dc8738ca12f5c
Esther Brown <egiles@una.edu>
2022-03-14 19:53:05 -0500

    Update index.php

--- Application/index.php
+++ Application/index.php
@@ -14,19 +14,19 @@
     }   
 
 // PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:unasnap.database.windows.net,1433; Database = lionappsql", "unasnap", "Leoluna2022");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+// try {
+//     $conn = new PDO("sqlsrv:server = tcp:unasnap.database.windows.net,1433; Database = lionappsql", "unasnap", "Leoluna2022");
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// }
+// catch (PDOException $e) {
+//     print("Error connecting to SQL Server.");
+//     die(print_r($e));
+// }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "unasnap", "pwd" => "Leoluna2022", "Database" => "lionappsql", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:unasnap.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+// // SQL Server Extension Sample Code:
+// $connectionInfo = array("UID" => "unasnap", "pwd" => "Leoluna2022", "Database" => "lionappsql", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+// $serverName = "tcp:unasnap.database.windows.net,1433";
+// $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
 
 
