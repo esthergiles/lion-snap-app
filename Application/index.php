acc335de7b94774da052f1e7ec1f287902eefec2
Esther Giles <47467263+esthergiles@users.noreply.github.com>
2022-03-22 18:04:45 -0500

    Update index.php

--- Application/index.php
+++ Application/index.php
@@ -13,6 +13,25 @@
       $message = "Success! You entered: ".$input;
     }   
 
+$host = 'demo-server.mysql.database.azure.com';
+$username = 'demoserveradmin';
+$password = 'sWq*75AMm';
+$db_name = 'demo';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+// mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
 
 ?>
 
