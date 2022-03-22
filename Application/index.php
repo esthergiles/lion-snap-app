de64f1282328f9c13a38b86255fa1d44e8ef4c53
Esther Giles <47467263+esthergiles@users.noreply.github.com>
2022-03-22 13:09:30 -0500

    Delete Database test

--- Application/index.php
+++ Application/index.php
@@ -13,18 +13,6 @@
       $message = "Success! You entered: ".$input;
     }   
 
-    $servername = "unasnap.database.windows.net";
-$username = "unasnap";
-$password = "s'Wq*75AM,";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
 
 // PHP Data Objects(PDO) Sample Code:
 // try {
