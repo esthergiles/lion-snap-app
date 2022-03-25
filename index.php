793243bcfb157780471d20e05b11e77220a5a5c5
Esther Brown <egiles@una.edu>
2022-03-24 20:20:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -25,17 +25,17 @@ if (mysqli_connect_errno())
 
 // Run the create table query
 
-if (mysqli_query($conn, '
-CREATE TABLE Products (
-`Id` INT NOT NULL AUTO_INCREMENT ,
-`ProductName` VARCHAR(200) NOT NULL ,
-`Color` VARCHAR(50) NOT NULL ,
-`Price` DOUBLE NOT NULL ,
-PRIMARY KEY (`Id`)
-);
-')) {
-printf("Table created\n");
-}
+// if (mysqli_query($conn, '
+// CREATE TABLE Products (
+// `Id` INT NOT NULL AUTO_INCREMENT ,
+// `ProductName` VARCHAR(200) NOT NULL ,
+// `Color` VARCHAR(50) NOT NULL ,
+// `Price` DOUBLE NOT NULL ,
+// PRIMARY KEY (`Id`)
+// );
+// ')) {
+// printf("Table created\n");
+// }
 
 //Create an Insert prepared statement and run it
 $product_name = 'BrandNewProduct';
