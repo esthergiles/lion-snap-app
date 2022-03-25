306d354ca97cfab3cc547be28464d985eced4248
Esther Brown <egiles@una.edu>
2022-03-24 21:28:18 -0500

    adding new file to print
    adding new file for printing data

--- index.php
+++ index.php
@@ -1,69 +1,3 @@
-<?php
-    // Postback
-    $message = "";
-    if(isset($_POST['SubmitButton'])){ //check if form was submitted
-      $input = $_POST['inputText']; //get input text
-      $message = "Success! You entered: ".$input;
-    }   
-
-$host = 'lionsnap-server.mysql.database.azure.com';
-$username = 'taamhhvhmx';
-$password = 'E7HHMN348V848DA8*';
-$db_name = 'testconnect';
-
-//Initializes MySQLi
-$conn = mysqli_init();
-
-// Establish the connection
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
-
-//If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
-// Run the create table query
-
-// if (mysqli_query($conn, '
-// CREATE TABLE StudentNames (
-// `Id` INT NOT NULL AUTO_INCREMENT ,
-// `EnteredName` VARCHAR(200) NOT NULL ,
-// PRIMARY KEY (`Id`)
-// );
-// ')) {
-// printf("Table created\n");
-// }
-
-        //Create an Insert prepared statement and run it
-        $new_name = $_POST['name'];
-        if ($stmt = mysqli_prepare($conn, "INSERT INTO StudentNames (EnteredName) VALUES (?)"))
-        {
-            mysqli_stmt_bind_param($stmt, 'ssd', $new_name);
-            mysqli_stmt_execute($stmt);
-            printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-            mysqli_stmt_close($stmt);
-        }
-        //Run the Select query
-        printf("Names Entered: \n");
-        $res = mysqli_query($conn, 'SELECT * FROM StudentNames');
-        while ($row = mysqli_fetch_assoc($res))
-        {
-            echo $row. "<br>";
-        }
-        
-//Run the Delete statement
-// $product_name = 'BrandNewProduct';
-// if ($stmt = mysqli_prepare($conn, "DELETE FROM Products WHERE ProductName = ?")) {
-// mysqli_stmt_bind_param($stmt, 's', $product_name);
-// mysqli_stmt_execute($stmt);
-// printf("Delete: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-// mysqli_stmt_close($stmt);
-// }
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -79,7 +13,7 @@ if (mysqli_connect_errno())
             <div class="success-text">UNA SNAP Application UI</div>
         </div>  
 
-        <form action="" method="post">
+        <form action="print.php" method="post">
         Enter your name: 
         <input type="text" name="name"/>
         <input type="submit" name="SubmitButton"/>
---
+++ print.php
@@ -0,0 +1,47 @@
+<?php
+$new_name = $_POST['name'];
+
+
+$host = 'lionsnap-server.mysql.database.azure.com';
+$username = 'taamhhvhmx';
+$password = 'E7HHMN348V848DA8*';
+$db_name = 'testconnect';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+//Create an Insert prepared statement and run it
+if ($stmt = mysqli_prepare($conn, "INSERT INTO StudentNames (EnteredName) VALUES (?)"))
+{
+    mysqli_stmt_bind_param($stmt, 'ssd', $new_name);
+    mysqli_stmt_execute($stmt);
+    printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+    mysqli_stmt_close($stmt);
+}
+//Run the Select query
+printf("Names Entered: \n");
+$res = mysqli_query($conn, 'SELECT * FROM StudentNames');
+while ($row = mysqli_fetch_assoc($res))
+{
+    echo $row. "<br>";
+}
+//Run the Delete statement
+// $product_name = 'BrandNewProduct';
+// if ($stmt = mysqli_prepare($conn, "DELETE FROM Products WHERE ProductName = ?")) {
+// mysqli_stmt_bind_param($stmt, 's', $product_name);
+// mysqli_stmt_execute($stmt);
+// printf("Delete: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+// mysqli_stmt_close($stmt);
+// }
+
+?>
