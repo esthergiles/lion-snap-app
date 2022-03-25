4313a22f919b68a3a6fbdc324fd7c4f00fa6734e
Esther Brown <egiles@una.edu>
2022-03-24 20:13:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -23,32 +23,27 @@ if (mysqli_connect_errno())
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
+// Run the create table query
+
 if (mysqli_query($conn, '
-CREATE TABLE testTable (
+CREATE TABLE Products (
 `Id` INT NOT NULL AUTO_INCREMENT ,
-`TestName` VARCHAR(200) NOT NULL ,
-`Phone` VARCHAR(50) NOT NULL ,
-`Email` VARCHAR(50) NOT NULL ,
-`TestStart` VARCHAR(50) NOT NULL ,
-`TestStop` VARCHAR(50) NOT NULL ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
 PRIMARY KEY (`Id`)
 );
 ')) {
 printf("Table created\n");
 }
 
-//Insert Statement
-
-$student_name = 'Test Name';
-$student_phone = 'Blue';
-$student_email = 'test email';
-$student_start = 'test start';
-$student_stop = 'test stop';
-
-
-if ($stmt = mysqli_prepare($conn, "INSERT INTO testTable (TestName, Phone, Email, TestStart, TestStop) VALUES (?, ?, ?, ?, ?)"))
+//Create an Insert prepared statement and run it
+$product_name = 'BrandNewProduct';
+$product_color = 'Blue';
+$product_price = 15.5;
+if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)"))
 {
-    mysqli_stmt_bind_param($stmt, 'ssd', $student_name, $student_phone, $student_email, $student_start, $student_stop);
+    mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
     mysqli_stmt_execute($stmt);
     printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
     mysqli_stmt_close($stmt);
@@ -56,11 +51,21 @@ if ($stmt = mysqli_prepare($conn, "INSERT INTO testTable (TestName, Phone, Email
 
 //Run the Select query
 printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM test');
+$res = mysqli_query($conn, 'SELECT * FROM Products');
 while ($row = mysqli_fetch_assoc($res))
  {
     var_dump($row);
  }
+
+
+//Run the Delete statement
+$product_name = 'BrandNewProduct';
+if ($stmt = mysqli_prepare($conn, "DELETE FROM Products WHERE ProductName = ?")) {
+mysqli_stmt_bind_param($stmt, 's', $product_name);
+mysqli_stmt_execute($stmt);
+printf("Delete: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+mysqli_stmt_close($stmt);
+}
 
 ?>
 
