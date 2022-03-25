f10b90a4ea91dc3737b789f48a07d59496c238ad
Esther Brown <egiles@una.edu>
2022-03-24 21:09:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -25,38 +25,15 @@ if (mysqli_connect_errno())
 
 // Run the create table query
 
-// if (mysqli_query($conn, '
-// CREATE TABLE Products (
-// `Id` INT NOT NULL AUTO_INCREMENT ,
-// `ProductName` VARCHAR(200) NOT NULL ,
-// `Color` VARCHAR(50) NOT NULL ,
-// `Price` DOUBLE NOT NULL ,
-// PRIMARY KEY (`Id`)
-// );
-// ')) {
-// printf("Table created\n");
-// }
-
-//Create an Insert prepared statement and run it
-$product_name = 'BrandNewProduct2';
-$product_color = 'Blue2';
-$product_price = 15.5;
-if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)"))
-{
-    mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
-    mysqli_stmt_execute($stmt);
-    printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-    mysqli_stmt_close($stmt);
+if (mysqli_query($conn, '
+CREATE TABLE StudentNames (
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`EnteredName` VARCHAR(200) NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
 }
-
-//Run the Select query
-printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Products');
-while ($row = mysqli_fetch_assoc($res))
- {
-    var_dump($row);
- }
-
 
 //Run the Delete statement
 // $product_name = 'BrandNewProduct';
@@ -86,22 +63,32 @@ while ($row = mysqli_fetch_assoc($res))
         </div>  
 
         <form action="" method="post">
-        Enter your favorite color: 
-        <input type="text" name="inputText"/>
+        Enter your name: 
+        <input type="text" name="name"/>
         <input type="submit" name="SubmitButton"/>
         </form> 
-        <div><?php echo $message; ?> </div>
 
+        <div>
         <?php
-        //Insert Color into database
-        // if ($stmt = mysqli_prepare($conn, "INSERT INTO Colors (Color) VALUES (?)"))
-        // {
-        //     mysqli_stmt_bind_param($stmt, 'ssd', $input);
-        //     mysqli_stmt_execute($stmt);
-        //     printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-        //     mysqli_stmt_close($stmt);
-        // }
+
+        //Create an Insert prepared statement and run it
+        $new_name = $_POST['name'];
+        if ($stmt = mysqli_prepare($conn, "INSERT INTO StudentNames (EnteredName) VALUES (?)"))
+        {
+            mysqli_stmt_bind_param($stmt, 'ssd', $new_name);
+            mysqli_stmt_execute($stmt);
+            printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+            mysqli_stmt_close($stmt);
+        }
+        //Run the Select query
+        printf("Names Entered: \n");
+        $res = mysqli_query($conn, 'SELECT * FROM StudentNames');
+        while ($row = mysqli_fetch_assoc($res))
+        {
+            echo $row. "<br>";
+        }
         ?>
+        </div>
     </div>
 
     
