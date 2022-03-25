4ce8c0e6671f71c0e90d7c242f4916d8866fd9df
Esther Brown <egiles@una.edu>
2022-03-24 19:27:59 -0500

    Update index.php
    Creating test tabe

--- index.php
+++ index.php
@@ -14,8 +14,6 @@ $db_name = 'testconnect';
 //Initializes MySQLi
 $conn = mysqli_init();
 
-// mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-
 // Establish the connection
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 
@@ -25,64 +23,20 @@ if (mysqli_connect_errno())
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-// // sql to create table
-// $sql = "CREATE TABLE MyGuests (
-//     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//     firstname VARCHAR(30) NOT NULL,
-//     lastname VARCHAR(30) NOT NULL,
-//     email VARCHAR(50),
-//     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-//     )";
-    
-//     if ($conn->query($sql) === TRUE) {
-//       echo "Table MyGuests created successfully";
-//     } else {
-//       echo "Error creating table: " . $conn->error;
-//     }
-
-// Run the create table query
 if (mysqli_query($conn, '
-CREATE TABLE Products (
+CREATE TABLE test (
 `Id` INT NOT NULL AUTO_INCREMENT ,
-`ProductName` VARCHAR(200) NOT NULL ,
-`Color` VARCHAR(50) NOT NULL ,
-`Price` DOUBLE NOT NULL ,
+`Name` VARCHAR(200) NOT NULL ,
+`Phone` VARCHAR(50) NOT NULL ,
+`Email` VARCHAR(50) NOT NULL ,
+`Start` VARCHAR(50) NOT NULL ,
+`Stop` VARCHAR(50) NOT NULL ,
 PRIMARY KEY (`Id`)
 );
 ')) {
 printf("Table created\n");
 }
 
-
-//Create an Insert prepared statement and run it
-$product_name = 'BrandNewProduct';
-$product_color = 'Blue';
-$product_price = 15.5;
-if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)"))
-{
-    mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
-    mysqli_stmt_execute($stmt);
-    printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-    mysqli_stmt_close($stmt);
-}
-
-//Run the Select query
-printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Products');
-while ($row = mysqli_fetch_assoc($res))
- {
-    var_dump($row);
- }
-
-
-//Run the Delete statement
-$product_name = 'BrandNewProduct';
-if ($stmt = mysqli_prepare($conn, "DELETE FROM Products WHERE ProductName = ?")) {
-mysqli_stmt_bind_param($stmt, 's', $product_name);
-mysqli_stmt_execute($stmt);
-printf("Delete: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-mysqli_stmt_close($stmt);
-}
 ?>
 
 
@@ -102,11 +56,22 @@ mysqli_stmt_close($stmt);
         </div>  
 
         <form action="" method="post">
-        <?php echo $message; ?>
         Enter your favorite color: 
         <input type="text" name="inputText"/>
         <input type="submit" name="SubmitButton"/>
         </form> 
+        <div><?php echo $message; ?> </div>
+
+        <?php
+        //Insert Color into database
+        // if ($stmt = mysqli_prepare($conn, "INSERT INTO Colors (Color) VALUES (?)"))
+        // {
+        //     mysqli_stmt_bind_param($stmt, 'ssd', $input);
+        //     mysqli_stmt_execute($stmt);
+        //     printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+        //     mysqli_stmt_close($stmt);
+        // }
+        ?>
     </div>
 
     
