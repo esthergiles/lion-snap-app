ef6b15eba1f3c989653e017e75f936c6b21f1708
Esther Brown <egiles@una.edu>
2022-04-04 18:07:35 -0500

    Update print.php
    adding some print outs

--- print.php
+++ print.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Lion Snap App Final</title>
+	<link href="css/site.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="main-container">
+        <div class="content-body">
+            <div class="success-text">You Entered</div>
+        </div>  
 <?php
-Print page
+  echo $_POST['name'];
 ?>
+
+    
+</body>
+</html>
