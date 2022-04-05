0f02bcbcab3d0747212703c6ff3582e0babc1889
Esther Brown <egiles@una.edu>
2022-04-04 22:58:02 -0500

    Update print.php
    print to test database connection

--- print.php
+++ print.php
@@ -25,7 +25,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";    
+// Insert Data
+// $conn->query()
 ?>
 
     
