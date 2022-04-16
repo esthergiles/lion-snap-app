12f639dd9f6c57df8c9784480118e70ec313d79b
Esther Brown <egiles@una.edu>
2022-04-16 15:47:42 -0500

    nav
    nav options

--- index.php
+++ index.php
@@ -16,6 +16,22 @@
   <a class="navbar-brand" href="https://www.una.edu">
     <img src="https://una.elluciancrmrecruit.com/Apply/File/DownloadPicture?name=una_logo2" width= "275px">
   </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">New Request</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Manage Existing Request</a>
+      </li>
+    </ul>
+  </div>
 </nav>
 
 
