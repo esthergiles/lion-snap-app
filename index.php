56a23f51937895a5283dc68b42e59450c4fe1da1
Esther Brown <egiles@una.edu>
2022-04-16 16:52:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,26 +12,6 @@
 </head>
 <body>
 <!-- Just an image -->
-<nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="https://www.una.edu">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">New Request</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Managed Existing Request</a>
-      </li>
-    </ul>
-  </div>
-</nav>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="https://www.una.edu"> <img src="https://una.elluciancrmrecruit.com/Apply/File/DownloadPicture?name=una_logo2" width= "275px">
@@ -48,6 +28,7 @@
     </div>
   </div>
 </nav>
+
 
             <?php
             //Step 1: Connect to DB
