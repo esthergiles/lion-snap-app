ad0adc1406197b4f7e2569fd616dfa97e63eb4c0
Esther Brown <egiles@una.edu>
2022-04-16 21:40:27 -0500

    Manage Request
    Created new page to manage user's requests by searching for existing requests and diisplaying them to the page. TOD:  add feature to update and delete Request

--- manage.php
+++ manage.php
@@ -38,11 +38,11 @@
       <li class="nav-item">
         <a class="nav-link" href="index.php">Home </a>
       </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="request_form.php">New Request<span class="sr-only">(current)</span></a>
+      <li class="nav-item ">
+        <a class="nav-link" href="request_form.php">New Request</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="manage.php">Managed Existing Request</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Managed Existing Request<span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
@@ -55,26 +55,15 @@
         <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
             <div class="card">
             <h3>STUDENT NIGHTTIME AUXILIARY PATROL</h3>
-            <p class="blue-text">SNAP provides nightly escorts anywhere on campus to persons on request.</p>
+            <p class="blue-text">Use this form to search for SNAP Escort Requests to update.</p>
 
-                <h5 class="text-center mb-4">Escort Request Details</h5>
-                <form class="form-card"  action ="insert.php" method="POST">
+                <h5 class="text-center mb-4">Manage Escort Request</h5>
+                <form class="form-card"  action ="display_requests.php" method="POST">
                     <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">First name<span class="text-danger"> *</span></label> <input type="text" id="fname" name="fname" placeholder="Enter your first name" onblur="validate(1)"> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Last name<span class="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="Enter your last name" onblur="validate(2)"> </div>
+                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Email Associated With Request<span class="text-danger"> *</span></label> <input type="text" id="email" name="email" placeholder="Enter email" onblur="validate(1)"> </div>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">UNA Email<span class="text-danger"> *</span></label> <input type="text" id="email" name="email" placeholder="" onblur="validate(3)"> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Phone number<span class="text-danger"> *</span></label> <input type="text" id="mob" name="mob" placeholder="" onblur="validate(4)"> </div>
-                    </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Pickup Location<span class="text-danger"> *</span></label> <input type="text" id="start_location" name="start_location" placeholder="" onblur="validate(5)"> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Dropoff Location<span class="text-danger"> *</span></label> <input type="text" id="end_location" name="end_location" placeholder="" onblur="validate(6)"> </div>                    </div>
-                                        <div class="row justify-content-between text-left">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Number of Students<span class="text-danger"> *</span></label> <input type="text" id="num" name="num" placeholder="" onblur="validate(7)"> </div>
-                    </div>
-                    <div class="row justify-content-end">
-                        <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Submit Request</button> </div>
+                     <div class="row justify-content-end">
+                        <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Find Requests</button> </div>
                     </div>
                 </form>
             </div>
