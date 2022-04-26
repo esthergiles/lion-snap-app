9c28b3b3e24c5e40f00177e7d79ae6317d0664fa
Esther Giles <47467263+esthergiles@users.noreply.github.com>
2022-04-26 14:12:39 -0500

    Replaced index
    replaced index with login form

--- index.php
+++ index.php
@@ -1,87 +1,166 @@
-<html>
-<head>
-       <!-- Required meta tags -->
-   <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
+<style>
+* { box-sizing: border-box; }
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="request_form_style.css">
-    <link rel="stylesheet" href="style.css">
 
-    <title>&nbsp; UNA SNAP </title>
+body {
+  font-family: 'Rubik', sans-serif;
+  background-color: lightgoldenrodyellow;
+}
 
-</head>
+.container {
+  display: flex;
+  height: 100vh;
+}
 
-<body>
-  <!-- Scripts for Bootstrap -->
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+.left {
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  animation-name: left;
+  animation-duration: 1s;
+  animation-fill-mode: both;
+  animation-delay: 1s;
+}
 
-<!-- Nav Bar -->
-<nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="https://www.una.edu">
-    <img src="https://una.elluciancrmrecruit.com/Apply/File/DownloadPicture?name=una_logo2" width= "275px">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="about.html">About </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="request_form.php">New Request<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="manage.php">Managed Existing Request</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+.right {
+  flex: 1;
+  background-color: black;
+  transition: 1s;
+  background-image: url(https://theflorala.com/wp-content/uploads/2020/10/Lions-den-1-900x600.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
 
-<body>
-<!-- TODO update form action to send to insert. -->
-<div class="container-fluid px-1 py-5 mx-auto">
-    <div class="row d-flex justify-content-center">
-        <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
-            <div class="card">
-            <h3>STUDENT NIGHTTIME AUXILIARY PATROL</h3>
-            <p class="blue-text">SNAP provides nightly escorts anywhere on campus to persons on request.</p>
 
-                <h5 class="text-center mb-4">Escort Request Details</h5>
-                <form class="form-card"  action ="insert.php" method="POST">
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">First name<span class="text-danger"> *</span></label> <input type="text" id="fname" name="fname" placeholder="Enter your first name" onblur="validate(1)" required> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Last name<span class="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="Enter your last name" onblur="validate(2)" required> </div>
-                    </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">UNA Email<span class="text-danger"> *</span></label> <input type="email" id="email" name="email" placeholder="email@una.edu" onblur="validate(3)" required> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Phone number<span class="text-danger"> *</span></label> <input type="tel" id="mob" name="mob" placeholder="123-456-7899" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" onblur="validate(4)" required> </div>
-                    </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Pickup Location<span class="text-danger"> *</span></label> <input type="text" id="start_location" name="start_location" onblur="validate(5)" required> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Dropoff Location<span class="text-danger"> *</span></label> <input type="text" id="end_location" name="end_location" placeholder="Enter End Location" onblur="validate(6)" required> </div>                    </div>
-                                        <div class="row justify-content-between text-left">
-                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Number of Students<span class="text-danger"> *</span></label> <input type="number" min="1" max ="10" id="num" name="num" placeholder="2" onblur="validate(7)" required> </div>
-                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Date & Time <span class="text-danger"> *</span></label> <input type="datetime-local" id="num" name="datetime" placeholder="" onblur="validate(7)"> </div>
 
-                    </div>
-                    <div class="row justify-content-end">
-                        <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Submit Request</button> </div>
-                    </div>
-                </form>
-            </div>
-        </div>
+.header > h2 {
+  margin: 0;
+  color: #4f46a5;
+}
+
+.header > h4 {
+  margin-top: 10px;
+  font-weight: normal;
+  font-size: 15px;
+  color: rgba(0,0,0,.4);
+}
+
+.form {
+  max-width: 80%;
+  display: flex;
+  flex-direction: column;
+}
+
+.form > p {
+  text-align: right;
+}
+
+.form > p > a {
+  color: #000;
+  font-size: 14px;
+}
+
+.form-field {
+  height: 46px;
+  padding: 0 16px;
+  border: 2px solid purple;
+  background-color: lightgoldenrodyellow;
+  border-radius: 4px;
+  font-family: 'Rubik', sans-serif;
+  outline: 0;
+  transition: .2s;
+  margin-top: 20px;
+}
+
+.form-field:focus {
+  border-color: #0f7ef1;
+}
+
+.form > button {
+  padding: 12px 10px;
+  border: 0;
+  background: gold; 
+  border-radius: 3px;
+  margin-top: 10px;
+  color: purple;
+  letter-spacing: 1px;
+  font-family: 'Rubik', sans-serif;
+}
+
+.animation {
+  animation-name: move;
+  animation-duration: .4s;
+  animation-fill-mode: both;
+  animation-delay: 2s;
+}
+
+.a1 {
+  animation-delay: 2s;
+}
+
+.a2 {
+  animation-delay: 2.1s;
+}
+
+.a3 {
+  animation-delay: 2.2s;
+}
+
+.a4 {
+  animation-delay: 2.3s;
+}
+
+.a5 {
+  animation-delay: 2.4s;
+}
+
+.a6 {
+  animation-delay: 2.5s;
+}
+
+@keyframes move {
+  0% {
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(-40px);
+  }
+
+  100% {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0);
+  }
+}
+
+@keyframes left {
+  0% {
+    opacity: 0;
+    width: 0;
+  }
+
+  100% {
+    opacity: 1;
+    padding: 20px 40px;
+    width: 440px;
+  }
+}
+</style>
+
+<div class="container">
+  <div class="left">
+    <div class="header">
+      <h2 class="animation a1">Welcome to SNAP</h2>
+      <h4 class="animation a2">Log in to your account using UNA Portal email and password</h4>
     </div>
+    <div class="form">
+      <input type="email" class="form-field animation a3" placeholder="Email Address">
+      <input type="password" class="form-field animation a4" placeholder="Password">
+      <p class="animation a5"><a href="#">Forgot Password</a></p>
+      <button type="submit" class="animation a6"><a href="request_form.php"> LOGIN</a></button>
+    </div>
+  </div>
+  <div class="right"></div>
 </div>
-</body>
-</html>
